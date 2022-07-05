9714f538bddebff7b2897d85aca889b0bae9c47d
Anda Toshiki <101481353+andatoshiki@users.noreply.github.com>
2022-07-05 08:28:14 +0800

    feat(up2one): updated latest upload to onedrive sh

--- up2one.sh
+++ up2one.sh
@@ -11,26 +11,29 @@
 # license: MIT
 #=================================================
 
-path=$3
-downloadpath="/usr/local/caddy/www/aria2/Download" # here's where your download path is set
+#!/bin/bash
+path=$3 #取原始路径，我的环境下如果是单文件则为/data/demo.png,如果是文件夹则该值为文件夹内某个文件比如/data/a/b/c/d.jpg
+downloadpath='/usr/local/caddy/www/aria2/Download'  #修改成Aria2下载文件夹
+domain='moerats.com'  #修改成自己域名
+
 if [ $2 -eq 0 ]
-    then
-        exit 0
+        then
+                exit 0
 fi
-while true; do
+while true; do  #提取下载文件根路径，如把/data/a/b/c/d.jpg变成/data/a
 filepath=$path
-path=${path%/*};
-if [ "$path" = "$downloadpath" ] && [ $2 -eq ]
-    then # here is where your php binary is located
-        /www/server/php/74/bin/php /data/wwwroot/upload.toshiki.top/one.php upload:file # upload type=file for specifying file uploads
-"$filepath" /upload/ # choose your designated upload folder for OneDrive and make sure it exists (create the folder first)
-    rm -rf "$filepath"
-    exit 0
-elif [ "$path"="$downloadpath" ]
+path=${path%/*}; 
+if [ "$path" = "$downloadpath" ] && [ $2 -eq 1 ]  #如果下载的是单个文件
     then
-        /www/server/php/74/bin/php /data/wwwroot/upload.toshiki.top/one.php upload:folder # upload type=folder for specifying folder uploads
-"$filepath"/ /upload/"${filepath##*/}"/
-    rm -rf "$filepath"/
-    exit -
+    php /www/wwwroot/$domain/one.php upload:file $filepath /$folder/
+    rm -rf $filepath
+    php /www/wwwroot/$domain/one.php cache:refresh
+    exit 0
+elif [ "$path" = "$downloadpath" ]
+    then
+    php /www/wwwroot/$domain/one.php upload:folder $filepath /$folder/
+    rm -rf "$filepath/"
+    php /www/wwwroot/$domain/one.php cache:refresh
+    exit 0
 fi
 done
