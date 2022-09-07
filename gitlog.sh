6a6634ecd92e906dd9b9d4ee99d9a0fb2908a5bd
andatoshiki <a_toshiki@outlook.com>
2022-09-07 20:06:56 +0800

    feat(gitlog)!: added gitlog script for displaying git log

---
+++ gitlog.sh
@@ -0,0 +1,7 @@
+# =========================================
+# author: @andatoshiki
+# version# v0.0.1
+# description: beautify git log on terminal
+# =========================================
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+
